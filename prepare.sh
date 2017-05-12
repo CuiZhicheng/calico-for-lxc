01022a3dd0063965f584ba07a1d818f8ded4c676
root <czc92914@163.com>
2017-05-12 00:41:02 -0400

    fix bug

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 apt-get update
-apt-get install docker etcd
+apt-get -y install docker docker.io lxc etcd
 mkdir -p /opt/bin
 mkdir -p /etc/calico
 mkdir -p /etc/cni/net.d
 chmod a+w -R /etc/cni/net.d
 cp tool/* /opt/bin/
-chmod +x opt/bin/calico
-chmod +x opt/bin/calico-ipam
-chmod +x opt/bin/calicoctl
-chmod +x opt/bin/cnitool
+chmod +x /opt/bin/calico
+chmod +x /opt/bin/calico-ipam
+chmod +x /opt/bin/calicoctl
+chmod +x /opt/bin/cnitool
 export PATH=$PATH:/opt/bin
